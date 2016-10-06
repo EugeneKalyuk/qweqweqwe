64702a1ca2c72f7a2de88f81b6747b46a7f0a96b
kaliuk.evgeny@yandex.ru <kaliuk.evgeny@yandex.ru>
2016-10-06 22:05:05 +0700

    xnj yt nfr

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,32 @@
 'use strict';
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+
 module.exports = {
     entry: "./home",
     output: {
-        filename: "build.js"
+        filename: "build.js",
+        library: "home"
     },
-    watch: true,
+    watch: NODE_ENV == 'development',
     watchOption: {
         aggregateTimeout: 100
     },
-    devtool: "source-map"
+    devtool: NODE_ENV == 'development' ?  "cheap-inline-module-source-map" : null,
+
+    plugins: [
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(NODE_ENV)
+        })
+    ],
+
+    module: {
+            loaders: [{
+                test: /\.js$/,
+                loader: 'babel?presets[]=es2015'
+            }
+       ]
+    }
+
 };
